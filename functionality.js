8ce4aaa5f41dd12c589b1e12b57698d9db54e1cd
Dilsher <dilsherb@gmail.com>
2023-12-04 13:02:35 +0300

    placeholders added.

--- functionality.js
+++ functionality.js
@@ -1,12 +1,17 @@
 const recipes = [
   {
     name: "Achar Gosht",
-  ingredients: "Chicken 1/2 kg, Achar Gosht Masala, Zeera, Chopped Onion 1/4 kg, Cooking Oil 1/8 kg",
-  instructions: "Fry chicken in oil for 10 min, Add (Chopped Onion, Zeera and Achar Gosht Masala), Cook for 20 min"},
+    ingredients:
+      "Chicken 1/2 kg, Achar Gosht Masala, Zeera, Chopped Onion 1/4 kg, Cooking Oil 1/8 kg",
+    instructions:
+      "Fry chicken in oil for 10 min, Add (Chopped Onion, Zeera and Achar Gosht Masala), Cook for 20 min",
+  },
   {
     name: "Chicken Karahi",
-    ingredients: "Chicken 1/2 kg, Karahi Masala, Chopped Onion 1/4 kg, Cooking Oil 1/8 kg, Zeera",
-    instructions: "Fry chicken in oil for 10 min, Add (Chopped Onion, Karahi Masala), Cook for 20 min"
+    ingredients:
+      "Chicken 1/2 kg, Karahi Masala, Chopped Onion 1/4 kg, Cooking Oil 1/8 kg, Zeera",
+    instructions:
+      "Fry chicken in oil for 10 min, Add (Chopped Onion, Karahi Masala), Cook for 20 min",
   },
 ];
 
@@ -14,12 +19,18 @@ const displayRecipes = () => {
   const recipeList = document.getElementById("recipeList");
   recipeList.innerHTML = "";
   recipes.forEach((recipe, index) => {
-    const card = document.createElement('div');
+    const card = document.createElement("div");
     // card.classList.add('recipe-card');
-    const ingredientsList = recipe.ingredients.split(',').map(item => `<li>${item.trim()}</li>`).join('');
-    const instructionsList = recipe.instructions.split(',').map(item => `<li>${item.trim()}</li>`).join('');
+    const ingredientsList = recipe.ingredients
+      .split(",")
+      .map((item) => `<li>${item.trim()}</li>`)
+      .join("");
+    const instructionsList = recipe.instructions
+      .split(",")
+      .map((item) => `<li>${item.trim()}</li>`)
+      .join("");
     card.innerHTML += `
-    <div class="card" style="width: 18rem;">
+    <div class="card" style="width: 15rem;">
       <div class="card-body">
         <h5 class="card-title">${recipe.name}</h5>
         <h6 class="card-subtitle">Ingredients:</h6>
@@ -32,15 +43,15 @@ const displayRecipes = () => {
     </div>
     `;
     recipeList.appendChild(card);
-  })
-}
+  });
+};
 
 const addRecipe = () => {
   const name = document.querySelector("#recipeName").value;
   const ingredients = document.querySelector("#recipeIngredients").value;
   const instructions = document.querySelector("#recipeInstructions").value;
-  if(name && ingredients && instructions){
-    recipes.push({name, ingredients, instructions});
+  if (name && ingredients && instructions) {
+    recipes.push({ name, ingredients, instructions });
     displayRecipes();
     document.querySelector("#recipeName").value = "";
     document.querySelector("#recipeIngredients").value = "";
@@ -50,7 +61,7 @@ const addRecipe = () => {
   } else {
     alert("Please fill all the fields");
   }
-}
+};
 
 const editRecipe = (index) => {
   const name = document.querySelector("#recipeName");
@@ -65,13 +76,13 @@ const editRecipe = (index) => {
   const cellToFocus = document.querySelector("#recipeName");
   cellToFocus.focus();
   displayRecipes();
-}
+};
 
 function deleteRecipe(index) {
   const confirmation = confirm("Are you sure you want to delete this recipe?");
   if (confirmation) {
-      recipes.splice(index, 1);
-      displayRecipes();
+    recipes.splice(index, 1);
+    displayRecipes();
   }
 }
 
